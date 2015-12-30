0eab1bd8d9b6d5b84481e0b46307747e75ad89b4
Gero Walter <g.m.walter@tue.nl>
2015-12-30 19:13:44 +0100

    parameter sets plot

--- pdc-illu.R
+++ pdc-illu.R
@@ -18,6 +18,20 @@ nyupdate <- function (pr, data){
   c(nn,yn)
 }
 
+luck4cny <- function(luck, posterior=FALSE){
+  c1 <- c(n0(luck)[1], y0(luck)[1])
+  c2 <- c(n0(luck)[1], y0(luck)[2])
+  c3 <- c(n0(luck)[2], y0(luck)[1])
+  c4 <- c(n0(luck)[2], y0(luck)[2])
+  if(posterior){
+    c1 <- nyupdate(c1, c(tau(data(luck)), n(data(luck))))
+    c2 <- nyupdate(c2, c(tau(data(luck)), n(data(luck))))
+    c3 <- nyupdate(c3, c(tau(data(luck)), n(data(luck))))
+    c4 <- nyupdate(c4, c(tau(data(luck)), n(data(luck))))
+  }
+  list(c1=c1, c2=c2, c3=c3, c4=c4)
+}
+
 dbetany <- function(x, ny, ...){
   dbeta(x, shape1=ny[1]*ny[2], shape2=ny[1]*(1-ny[2]), ...)
 }
@@ -137,7 +151,8 @@ pdf("singleprior-pdc2.pdf", width=8, height=5)
 grid.arrange(betapdfcdf, pdcillupmfcmf, nrow=1, ncol=2, widths=c(1,1.2))
 dev.off()
 
-# ------------------- sets of Beta pdfs, sets of Beta-Bin cmfs -------------------------
+
+# ------------------- sets of Beta pdfs, parameter sets, sets of Beta-Binomial cmfs -------------------------
 
 setpr <- BinomialLuckModel(n0=c(1,8), y0=c(0.7, 0.8))
 setpos1 <- setpr
@@ -145,11 +160,12 @@ data(setpos1) <- BinomialData(s=12, n=16)
 setpos2 <- setpr
 data(setpos2) <- BinomialData(s= 0, n=16)
 
-betavec <- seq(0,1, length.out=200)
+# Beta cdfs
+betavec <- seq(0,1, length.out=201)
 
-cdfplot(setpos1, xvec=betavec)
-cdfplot(setpos1, xvec=betavec, control=controlList(posterior=TRUE))
-cdfplot(setpos2, xvec=betavec, control=controlList(posterior=TRUE))
+#cdfplot(setpos1, xvec=betavec)
+#cdfplot(setpos1, xvec=betavec, control=controlList(posterior=TRUE))
+#cdfplot(setpos2, xvec=betavec, control=controlList(posterior=TRUE))
 
 betapriolow <- sapply(betavec, function(x){
   optim(par=c(4, 0.7), fn=function(.n0y0, x){
@@ -165,6 +181,10 @@ betaprioupp <- sapply(betavec, function(x){
   }, method="L-BFGS-B", control=list(fnscale=-1),
   lower=c(n0(setpr)[1], y0(setpr)[1]),
   upper=c(n0(setpr)[2], y0(setpr)[2]), x=x)$value
+})
+
+betapriocorners <- sapply(luck4cny(setpr), function(x){
+  pbetany(betavec, x)
 })
 
 betapos1low <- sapply(betavec, function(x){
@@ -183,6 +203,10 @@ betapos1upp <- sapply(betavec, function(x){
   upper=c(n0(setpr)[2], y0(setpr)[2]), x=x)$value
 })
 
+betapos1corners <- sapply(luck4cny(setpos1, posterior=TRUE), function(x){
+  pbetany(betavec, x)
+})
+
 betapos2low <- sapply(betavec, function(x){
   optim(par=c(4, 0.7), fn=function(.n0y0, x){
     pbetany(x, nyupdate(pr=.n0y0, data=c(tau(data(setpos2)), n(data(setpos2)))))
@@ -199,11 +223,65 @@ betapos2upp <- sapply(betavec, function(x){
   upper=c(n0(setpr)[2], y0(setpr)[2]), x=x)$value
 })
 
-betasetdf <- rbind(data.frame(x=betavec, Lower=betapriolow, Upper=betaprioupp, Item="Prior"),
-                   data.frame(x=betavec, Lower=betapos1low, Upper=betapos1upp, Item="Posterior 1"),
-                   data.frame(x=betavec, Lower=betapos2low, Upper=betapos2upp, Item="Posterior 2"))
+betapos2corners <- sapply(luck4cny(setpos2, posterior=TRUE), function(x){
+  pbetany(betavec, x)
+})
+
+betasetdf <- rbind(data.frame(x=betavec, Lower=betapriolow, Upper=betaprioupp, betapriocorners, Item="Prior", Facet="Prior & Posterior 1"),
+                   data.frame(x=betavec, Lower=betapos1low, Upper=betapos1upp, betapos1corners, Item="Posterior 1", Facet="Prior & Posterior 1"),
+                   data.frame(x=betavec, Lower=betapriolow, Upper=betaprioupp, betapriocorners, Item="Prior", Facet="Prior & Posterior 2"),
+                   data.frame(x=betavec, Lower=betapos2low, Upper=betapos2upp, betapos2corners, Item="Posterior 2", Facet="Prior & Posterior 2"))
 betasetdf$Item <- ordered(betasetdf$Item, levels=c("Prior", "Posterior 1", "Posterior 2"))
+
 betaset1 <- ggplot(betasetdf) + geom_ribbon(aes(x=x, ymin=Lower, ymax=Upper, group=Item, colour=Item, fill=Item), alpha=0.3)
+betaset1 <- betaset1 +
+  geom_line(aes(x=x, y=c1, group=Item, colour=Item)) +
+  geom_line(aes(x=x, y=c2, group=Item, colour=Item)) +
+  geom_line(aes(x=x, y=c3, group=Item, colour=Item)) +
+  geom_line(aes(x=x, y=c4, group=Item, colour=Item))
+betaset1 <- betaset1 + facet_grid(Facet ~ .) + rightlegend + xlab(expression(p[t]^k)) + ylab("cdf")
 betaset1
+
+# parameter sets
+
+n0vec <- seq(n0(setpr)[1], n0(setpr)[2], length.out=201)
+prioparam <- data.frame(n0=n0vec, Lower=y0(setpr)[1], Upper=y0(setpr)[2])
+pos1param <- data.frame(n0=updateLuckN(n0vec, n(data(setpos1))),
+                        Lower=updateLuckY(n0vec, y0(setpos1)[1], tau(data(setpos1)), n(data(setpos1))),
+                        Upper=updateLuckY(n0vec, y0(setpos1)[2], tau(data(setpos1)), n(data(setpos1))))
+pos2param <- data.frame(n0=updateLuckN(n0vec, n(data(setpos2))),
+                        Lower=updateLuckY(n0vec, y0(setpos2)[1], tau(data(setpos2)), n(data(setpos2))),
+                        Upper=updateLuckY(n0vec, y0(setpos2)[2], tau(data(setpos2)), n(data(setpos2))))
+
+paramsetdf <- rbind(data.frame(prioparam, Item="Prior", Facet="Prior & Posterior 1"),
+                    data.frame(pos1param, Item="Posterior 1", Facet="Prior & Posterior 1"),
+                    data.frame(prioparam, Item="Prior", Facet="Prior & Posterior 2"),
+                    data.frame(pos2param, Item="Posterior 2", Facet="Prior & Posterior 2"))
+
+paramset1 <- ggplot(paramsetdf) + geom_ribbon(aes(x=n0, ymin=Lower, ymax=Upper, group=Item, colour=Item, fill=Item), alpha=0.3)
+paramset1 <- paramset1 + facet_grid(Facet ~ .) + guides(linetype="none", colour="none", fill="none")
+paramset1 <- paramset1 + xlab(expression(n^(0))) + ylab(expression(y^(0)))
+paramset1
+
+paramset1 <- paramset1 + theme(plot.margin = unit(c(0,0.5,0,-0.25), "lines"))
+betaset1  <- betaset1  + theme(plot.margin = unit(c(0,0,  0,-0.25), "lines"),
+                                         legend.margin = unit(-0.5, "cm"))
+# parameter sets and cdf sets in one plot
+pdf("priorset.pdf", width=8, height=5)
+grid.arrange(paramset1, betaset1, nrow=1, ncol=2, widths=c(1,1.2))
+dev.off()
+
+paramset2 <- ggplot(paramsetdf) + geom_ribbon(aes(x=n0, ymin=Lower, ymax=Upper, group=Item, colour=Item, fill=Item), alpha=0.3)
+paramset2 <- paramset2 + xlab(expression(n^(0))) + ylab(expression(y^(0)))
+paramset2
+
+# plot with parameter sets only
+pdf("paramsets.pdf", width=6, height=5)
+paramset2 + theme(plot.margin = unit(c(0,0.5,0,-0.25), "lines"), legend.margin = unit(-0.5, "cm"))
+dev.off()
+
+# sets of Beta-Binomial cmfs
+
+head(betasetdf)
 
 #
